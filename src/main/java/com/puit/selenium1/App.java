a236281593abba9e64eacbc3303b37548855eb65
poonams21 <poonammsingh21@gmail.com>
2018-05-12 21:51:16 +0530

    commiting Selenium project using maven

---
+++ src/main/java/com/puit/selenium1/App.java
@@ -0,0 +1,13 @@
+package com.puit.selenium1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
